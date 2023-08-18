f6c41a7f844f3604aed0e4d63e3f093fd1261102
osung_jihyun <61964114+Choi-Jihyun@users.noreply.github.com>
2023-08-18 14:17:59 +0900

    mobile link fix

--- src/mobile/components/MobileGallerySection.js
+++ src/mobile/components/MobileGallerySection.js
@@ -11,7 +11,7 @@ export default function GallerySection() {
           <p className='title'>Our best gallery from LoveSilver</p>
           <p className='subtitle'>다양한 활동들을 담은 사진으로 LoveSilver의 즐거운 일상을 엿볼 수 있습니다. 예술, 체조, 문화행사 등 다채로운 프로그램들을 통해 회원님들에게 행복을 선사하는 저희 센터에서 보낸 활기찬 시간을 갤러리에서 확인해보세요. 이곳에서는 회원님들의 삶에 즐거움과 활력을 불어넣는 순간들을 만나보실 수 있습니다.</p>
           <p className='green_button'>
-            <Link to= '/gallery'>구경하기</Link>
+            <Link to= '/mobilegallery'>구경하기</Link>
           </p>
         </div>
         <div className={styles.gallery_wrap}>
