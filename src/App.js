d5ee7390d3f0a8baae4ea1ac6d0f853396515cad
osung_jihyun <61964114+Choi-Jihyun@users.noreply.github.com>
2023-08-18 14:01:59 +0900

    router setting 오류 있으면 주석으로 돌아가면 된다

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { RouterProvider, createBrowserRouter, useRoutes } from 'react-router-dom';
 import './App.css';
 import Root from './pages/Root';
 import Home from './pages/Home';
@@ -20,59 +20,124 @@ import { useEffect, useState } from 'react';
 import { AuthContextProvider } from './context/AuthContext';
 
 
-function App() {
+// function App() {
 
+//   const [windowInnerWidth, setWindowInnerWidth] = useState(window.innerWidth);
+//   const handleResize = () => {
+//     setWindowInnerWidth(window.innerWidth);
+//   }
+//   useEffect(()=>{
+//     window.addEventListener('resize', handleResize);
+
+//     return () => {
+//       window.removeEventListener('resize', handleResize);
+//     };
+//   }, [])
+
+//   let router = null;
+
+//   if(isMobile || windowInnerWidth < 768) {
+//     router = createBrowserRouter([
+//       {
+//         path: '/',
+//         element: <MobileRoot/>,
+//         errorElement: <NotFound/>,
+//         children: [
+//           {index: true, element: <MobileHome/>},
+//           {path: '/mobilevisit/:placeId', element: <MobileVisit/>},
+//           {path: '/mobilecurriculum', element: <MobileCurriculum/>},
+//           {path: '/mobilegallery', element: <MobileGallery/>},
+//           {path: '/mobilegallery/:productId', element: <MobileGalleryDetail/>},
+//           {path: '/mobileinquire', element: <MobileInquire/>}
+//         ]
+//       }
+//     ])
+//   } else {
+//     router = createBrowserRouter([
+//       {
+//         path: '/',
+//         element: <Root/>,
+//         errorElement: <NotFound/>,
+//         children: [
+//           {index: true, element: <Home/>},
+//           {path: '/visit/:placeId', element: <Visit/>},
+//           {path: '/curriculum', element: <Curriculum/>},
+//           {path: '/gallery', element: <Gallery/>},
+//           {path: '/gallery/:productId', element: <GalleryDetail/>},
+//           {path: '/inquire', element: <Inquire/>}
+//         ]
+//       }
+//     ])
+//   }
+//   return (
+//     <div className="App">
+//       <AuthContextProvider>
+//         <RouterProvider router={router}/>
+//       </AuthContextProvider>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+function App() {
   const [windowInnerWidth, setWindowInnerWidth] = useState(window.innerWidth);
+
   const handleResize = () => {
     setWindowInnerWidth(window.innerWidth);
-  }
-  useEffect(()=>{
+  };
+
+  useEffect(() => {
     window.addEventListener('resize', handleResize);
 
     return () => {
       window.removeEventListener('resize', handleResize);
     };
-  }, [])
+  }, []);
 
-  let router = null;
+  // 라우트 설정
+  const routes = [
+  {
+    path: '/',
+    element: isMobile || windowInnerWidth < 768 ? <MobileRoot /> : <Root />,
+    errorElement: <NotFound />,
+    children: [
+      { index: true, element: isMobile || windowInnerWidth < 768 ? <MobileHome /> : <Home /> },
+      { 
+        path: isMobile || windowInnerWidth < 768 ? '/mobilevisit/:placeId' : '/visit/:placeId', 
+        element: isMobile || windowInnerWidth < 768 ? <MobileVisit /> : <Visit /> 
+      },
+      { 
+        path: isMobile || windowInnerWidth < 768 ? '/mobilecurriculum' : '/curriculum', 
+        element: isMobile || windowInnerWidth < 768 ? <MobileCurriculum /> : <Curriculum /> 
+      },
+      { 
+        path: isMobile || windowInnerWidth < 768 ? '/mobilegallery' : '/gallery', 
+        element: isMobile || windowInnerWidth < 768 ? <MobileGallery /> : <Gallery /> 
+      },
+      { 
+        path: isMobile || windowInnerWidth < 768 ? '/mobilegallery/:productId' : '/gallery/:productId', 
+        element: isMobile || windowInnerWidth < 768 ? <MobileGalleryDetail /> : <GalleryDetail /> 
+      },
+      { 
+        path: isMobile || windowInnerWidth < 768 ? '/mobileinquire' : '/inquire', 
+        element: isMobile || windowInnerWidth < 768 ? <MobileInquire /> : <Inquire /> 
+      },
+    ],
+  },
+];
 
-  if(isMobile || windowInnerWidth < 768) {
-    router = createBrowserRouter([
-      {
-        path: '/',
-        element: <MobileRoot/>,
-        errorElement: <NotFound/>,
-        children: [
-          {index: true, element: <MobileHome/>},
-          {path: '/mobilevisit/:placeId', element: <MobileVisit/>},
-          {path: '/mobilecurriculum', element: <MobileCurriculum/>},
-          {path: '/mobilegallery', element: <MobileGallery/>},
-          {path: '/mobilegallery/:productId', element: <MobileGalleryDetail/>},
-          {path: '/mobileinquire', element: <MobileInquire/>}
-        ]
-      }
-    ])
-  } else {
-    router = createBrowserRouter([
-      {
-        path: '/',
-        element: <Root/>,
-        errorElement: <NotFound/>,
-        children: [
-          {index: true, element: <Home/>},
-          {path: '/visit/:placeId', element: <Visit/>},
-          {path: '/curriculum', element: <Curriculum/>},
-          {path: '/gallery', element: <Gallery/>},
-          {path: '/gallery/:productId', element: <GalleryDetail/>},
-          {path: '/inquire', element: <Inquire/>}
-        ]
-      }
-    ])
-  }
+
+  // createBrowserRouter 사용하여 라우트 처리
+  const router = createBrowserRouter(routes);
+
   return (
     <div className="App">
       <AuthContextProvider>
-        <RouterProvider router={router}/>
+        <RouterProvider router={router}>
+          {router}
+        </RouterProvider>
       </AuthContextProvider>
     </div>
   );
