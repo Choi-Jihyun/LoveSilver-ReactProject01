279a8d885262068ff9b2b272ff62d208f7f97c42
osung_jihyun <61964114+Choi-Jihyun@users.noreply.github.com>
2023-08-21 10:44:24 +0900

    our best photos typo fix

--- src/mobile/components/MobileGallerySection.js
+++ src/mobile/components/MobileGallerySection.js
@@ -20,7 +20,7 @@ export default function GallerySection() {
       <section id={styles.gallery_section}>
       <h2 className='hidden'>활동 갤러리</h2>
         <div className={styles.text_wrap}>
-          <p className='title'>Our best gallery from LoveSilver</p>
+          <p className='title'>Our best photos from LoveSilver</p>
           <p className='subtitle'>다양한 활동들을 담은 사진으로 LoveSilver의 즐거운 일상을 엿볼 수 있습니다. 예술, 체조, 문화행사 등 다채로운 프로그램들을 통해 회원님들에게 행복을 선사하는 저희 센터에서 보낸 활기찬 시간을 갤러리에서 확인해보세요. 이곳에서는 회원님들의 삶에 즐거움과 활력을 불어넣는 순간들을 만나보실 수 있습니다.</p>
           <p className='green_button'>
             <Link to= '/mobilegallery'>구경하기</Link>
