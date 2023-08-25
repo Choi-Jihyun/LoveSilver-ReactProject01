5f9537f777669ad7b4688254144a9445f9bf6814
osung_jihyun <61964114+Choi-Jihyun@users.noreply.github.com>
2023-08-25 13:41:52 +0900

    update mobile admin design

--- src/pages/GalleryAddDetail.js
+++ src/pages/GalleryAddDetail.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import GalleryAddDetailInner from '../components/GalleryAddDetailInner'
 import BlackMenuBg from '../components/BlackMenuBg'
+import { isMobile } from 'react-device-detect';
 
 export default function GalleryAddDetail() {
   return (
     <div>
-      <BlackMenuBg />
+      <div style={{display: (isMobile ? 'none':'block')}}>
+        <BlackMenuBg/>
+      </div>
       <GalleryAddDetailInner />
     </div>
   )
